ebe927752e423f65b23cef96eb3d519d04e21df9
harishreddy-g <harishreddy.g23@gmail.com>
2026-03-02 12:09:47 +0530

    print statement

---
+++ printstate.java
@@ -0,0 +1,7 @@
+class printstate{
+    public static void main(String[] args) {
+    
+        System.out.println("Hello, World!");
+        System.out.print("Welcome to Java programming.");
+    }
+}
