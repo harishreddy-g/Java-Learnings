bbf30f175b0ccf923fd0a0f497e3c7d26ab8d420
harishreddy-g <harishreddy.g23@gmail.com>
2026-03-04 02:59:54 +0530

    For Loop

---
+++ forloop.java
@@ -0,0 +1,8 @@
+public class forloop {
+    public static void main(String[] args){
+        System.out.println("Numbers from 0 to 10:");
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
