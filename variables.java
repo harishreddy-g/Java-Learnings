ccec952681cb91bbca66f84adeb51b3ac8029fd7
harishreddy-g <harishreddy.g23@gmail.com>
2026-03-04 02:08:18 +0530

    java data types and variables

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args){
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number ! f is needed at the end of the number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;    //only true or false not 0,1
+        double myDoubleNum = 10.5d;     // Double
+        String myText = "Hello123";     // String
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+        System.out.println(myDoubleNum);
+    }
+}
