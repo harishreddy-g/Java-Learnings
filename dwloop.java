901e210dd89a1c5a46e7fea9425d94085ab73639
harishreddy-g <harishreddy.g23@gmail.com>
2026-03-04 03:03:00 +0530

    Do While Loop

---
+++ dwloop.java
@@ -0,0 +1,10 @@
+public class dwloop {
+    public static void main(String[] args){
+        int i=0;
+        System.out.println("Numbers from 0 to 10:");
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
