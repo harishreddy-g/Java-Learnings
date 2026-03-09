481f1a04018fcbdcb43156bf25dfcf11b9005248
harishreddy-g <harishreddy.g23@gmail.com>
2026-03-09 17:12:23 +0530

    Multi Dimensional Arrays

---
+++ multiarray.java
@@ -0,0 +1,18 @@
+public class multiarray {
+    public static void main(String[] args){
+        int[][] arr = new int[3][3];// decleration and memory allocation
+
+        int[][] arr1 ;// decleration
+
+        arr1 = new int[3][3];// memory allocation
+
+        int[][] arr2={{1,2,3},{2,1,3},{2,3,4}};//decleration and intialization 
+
+        int[] arr3[];// alternative way of decleration
+
+        int[][] arr4= new int[][]{{1,2,3},{2,1,3},{2,3,4}};// decleration, and intialization
+
+        int arr5[][];// alternative way of decleration
+
+    }
+}
