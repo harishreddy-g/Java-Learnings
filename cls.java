93715bb8b567ecbe7413f0c137261c556460fb54
harishreddy-g <harishreddy.g23@gmail.com>
2026-03-09 19:12:34 +0530

    Class and object

---
+++ cls.java
@@ -0,0 +1,14 @@
+class person{
+    String name;
+    int age;
+}
+
+public class cls {
+    public static void main(String[] args){
+        person p1 = new person();
+        p1.name = "harish";
+        p1.age= 20;
+        System.out.println("Hi , Iam " + p1.name + " and I am " + p1.age + " years old.");
+
+    }
+}
